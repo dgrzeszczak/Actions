c220d33912eba411e3f0f9f566d51f4b2c45a898
Greg Jurzak <greg.jurzak@mobile-5.com>
2018-12-06 13:35:47 +0100

    Add actions handlers.

--- Actions/Sources/ActionsHandlers.swift
+++ Actions/Sources/ActionsHandlers.swift
@@ -7,18 +7,12 @@
 //
 
 public protocol ActionHandler {
-
     associatedtype Act: Action
-
     func handle(param: Act.ParamType) -> Act.ReturnType
-
 }
 
 public protocol AsyncActionHandler {
-
     associatedtype Act: AsyncAction
-
     func handle(param: Act.ParamType, completion: @escaping (Act.ReturnType) -> Void)
-
 }
 
